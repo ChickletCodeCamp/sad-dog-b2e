7754b0914903560ca3c2ea2c0bce8340e3533735
Duke <hibary.taiwan@gmail.com>
2023-12-21 22:30:17 +0800

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; //swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with credentials
+  app.enableCors({
+    origin: true, // You can specify your frontend origin here if needed
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('sad dog API')
     .setDescription('Nest sad dog API description')
